9e727814ab391f504810a9053403bbfcba856ac7
Peter Palaga <ppalaga@redhat.com>
2017-10-17 15:27:30 +0200

    Rename SrcdepsLocalRepositoryManagerTest's package to org.srcdeps.mvn.itest

--- srcdeps-maven-quickstarts/src/test/java/org/srcdeps/mvn/itest/SrcdepsLocalRepositoryManagerTest.java
+++ srcdeps-maven-quickstarts/src/test/java/org/srcdeps/mvn/itest/SrcdepsLocalRepositoryManagerTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015-2016 Maven Source Dependencies
+ * Copyright 2015-2017 Maven Source Dependencies
  * Plugin contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.srcdeps.mvn.localrepo;
+package org.srcdeps.mvn.itest;
 
 import java.io.File;
 import java.io.IOException;
